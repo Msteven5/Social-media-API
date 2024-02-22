4cca62e9ee22324aaa549476cde43f3e011a2121
Michael Davidson <msdavid55@gmail.com>
2024-02-21 23:26:25 -0600

    initial commit with filing structure and models complete

---
+++ models/Thoughts.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+const Reaction = require('./Reaction')
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [Reaction]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+thoughtSchema
+  .virtual('reactionCount')
+  .get(function () {
+    return this.reactions.length;
+  });
+
+const thought = model('thought', thoughtSchema);
+
+module.exports = thought;
---
+++ models/index.js
@@ -0,0 +1,4 @@
+const Tags = require('./Thoughts');
+const User = require('./User');
+
+module.exports = { Tags, User };
